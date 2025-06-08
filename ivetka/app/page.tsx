b0a67eb72f10cd19e4831dcfd966da8e5bfb571e
Nazgul <koci.marek@icloud.com>
2025-06-08 19:08:24 +0200

    zmena uvod

--- ivetka/app/page.tsx
+++ ivetka/app/page.tsx
@@ -2,7 +2,7 @@ export default function HomePage() {
   return (
     <main className="min-h-screen flex items-center justify-center bg-gray-100">
       <h1 className="text-4xl font-bold text-gray-800">
-        Vítejte na našem realitním webu!
+        Test!
       </h1>
     </main>
   );
